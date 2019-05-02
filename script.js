81c875316d8b10f2226628b8ecc39db273bf56b0
Geetchopra <geet.s.chopra@gmail.com>
2019-05-02 17:28:48 +0530

    Design img modals

--- script.js
+++ script.js
@@ -8,7 +8,61 @@ function scrollFunction() {
   }
 }
 
-// When the user clicks on the button, scroll to the top of the document
+var modal = document.getElementById('designModal');
+
+// get design elements
+var grumble = document.getElementById('grumble');
+var scanscript = document.getElementById('scanscript');
+var pulse = document.getElementById('pulse');
+var chestnotesBrochure = document.getElementById('chestnotesBrochure');
+var chestnotesPoster = document.getElementById('flexposter1');
+var council = document.getElementById('flexposter2');
+
+var modalImg = document.getElementById("modalImg");
+grumble.onclick = function(){
+  modal.style.display = "block";
+  modalImg.src = "Assets/GS.jpg";
+}
+
+scanscript.onclick = function(){
+  modal.style.display = "block";
+  modalImg.src = "Assets/ScanScript.jpg";
+}
+
+pulse.onclick = function(){
+  modal.style.display = "block";
+  modalImg.src = "Assets/Pulse_Concept.png";
+}
+
+chestnotesBrochure.onclick = function(){
+  modal.style.display = "block";
+  modalImg.src = "Assets/Chestnotes.jpg";
+}
+
+chestnotesPoster.onclick = function(){
+  modal.style.display = "block";
+  modalImg.src = "Assets/ChestnotesPoster.jpg";
+  modalImg.style.width = "90%";
+}
+
+council.onclick = function(){
+  modal.style.display = "block";
+  modalImg.src = "Assets/iwantyou.jpg";
+  modalImg.style.width = "90%";
+}
+
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
+
+var span = document.getElementsByClassName("close")[0];
+
+span.onclick = function() { 
+  modal.style.display = "none";
+}
+
 function scrollToTop() {
   document.body.scrollTop = 0; // For Safari
   document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
